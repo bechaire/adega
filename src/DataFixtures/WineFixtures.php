1165feef389394b297825c21f9e8d7926c0f0b59
Paulo Marcelo Bechaire <bechaire@gmail.com>
2024-11-26 23:13:04 -0300

    Adicionado datafixtures bundle e criada carga de vinhos para testes e validação

---
+++ src/DataFixtures/WineFixtures.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DataFixtures;
+
+use App\Entity\Wine;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class WineFixtures extends Fixture implements FixtureGroupInterface
+{
+    public static function getGroups(): array { 
+        return ['radom_wines'];
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $grapes = ['Cabernet Sauvignon', 'Merlot', 'Pinot Noir', 'Chardonnay', 'Sauvignon Blanc'];
+        $countries = ['France', 'Italy', 'Spain', 'Australia', 'Chile'];
+        $volumes = [500, 600, 750, 900, 1000];
+
+        for ($i = 1; $i <= 10; $i++) {
+            $wine = new Wine();
+            $grape = $grapes[array_rand($grapes)];
+            $wine->setName("Wine {$i} ({$grape})")
+                ->setVolumeMl($volumes[array_rand($volumes)]) // 500ml ... 1000ml
+                ->setWeightKg(random_int(10, 20) / 10) // 1kg .. 2kg
+                ->setStock(random_int(2, 8))
+                ->setPrice(random_int(350, 2200) / 10)
+                ->setGrape($grape)
+                ->setCountry($countries[array_rand($countries)])
+                ->setAlcoholPerc(random_int(100, 160) / 10);
+            $manager->persist($wine);
+        }
+
+        $manager->flush();
+    }
+}
